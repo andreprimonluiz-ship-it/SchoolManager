7fb9268d353fa0c643965d502d3630d469124d06
Andre Luiz <andreprimonluiz@gmail.com>
2026-05-26 23:05:00 -0300

    Students details and delete

--- SchoolManager/Services/StudentsService.cs
+++ SchoolManager/Services/StudentsService.cs
@@ -1,5 +1,6 @@
 ﻿using SchoolManager.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace SchoolManager.Services
 {
@@ -19,6 +20,16 @@ namespace SchoolManager.Services
         public void Insert(Student obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+        public Student FindById(int id)
+        {
+            return _context.Students.Include(p => p.Classroom).FirstOrDefault(obj => obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Students.Find(id);
+            _context.Students.Remove(obj);
             _context.SaveChanges();
         }
 
